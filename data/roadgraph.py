a070bee47918d2a81bc60287c7242af6af8a0e01
jpthalman <jpthalman@gmail.com>
2024-02-07 23:11:44 -0800

    Populate roadgraph tensor

--- data/roadgraph.py
+++ data/roadgraph.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from typing import Iterator, List, Tuple
 
-import shapely
-import torch
 from av2.map.map_api import (
     ArgoverseStaticMap,
     PedestrianCrossing,
@@ -13,11 +11,13 @@ from av2.map.lane_segment import (
     LaneType,
 )
 from av2.map.map_primitives import Polyline
+import numpy as np
+import shapely
+import torch
 
 from data.dimensions import Dim
 
 
-MAX_DISTANCE = 300
 LANE_TYPE_ENCODING = {e: float(i) for i, e in enumerate(list(LaneType) + ["PED"])}
 LANE_MARK_ENCODING = {e: float(i) for i, e in enumerate(list(LaneMarkType))}
 
@@ -32,31 +32,37 @@ def extract(
     agent_history[A, T, 1, S]
     agent_mask[A, T]
     """
-    queries = []
-    for a in range(Dim.A):
-        t = _get_time_index(agent_mask, a)
-        state = agent_history[a, t, 0, :]
-        query = shapely.Point(
-            state[0] - reference_point[0],
-            state[1] - reference_point[1],
-        )
-        queries.append(query)
+    tree, data = _load_rtree(map_path, reference_point)
 
     roadgraph = torch.zeros([Dim.A, 1, Dim.R, Dim.Rd])
-    roadgraph_mask = torch.zeros([Dim.A, 1, Dim.R])
-
-    tree, data = _load_rtree(map_path, reference_point)
-    results = list(tree.query_nearest(queries, max_distance=MAX_DISTANCE))
+    roadgraph_mask = torch.zeros([Dim.A, 1, Dim.R]).bool()
     for a in range(Dim.A):
-        idx = results[a]
+        t = _get_time_index(agent_mask, a)
+        if t is None:
+            roadgraph_mask[a, 0, :] = True
+            continue
+
+        # Agent states are already in reference_point frame
+        query = shapely.Point(
+            agent_history[a, t, 0, 0],
+            agent_history[a, t, 0, 1],
+        )
+
+        idx = []
+        radius = 100.0
+        while len(idx) < min(Dim.R, len(data)):
+            idx = list(tree.query(query.buffer(radius)))
+            radius *= 2
+
         points = tree.geometries.take(idx)
-        agent_data = data.take(idx)
+        agent_data = data.take(idx, axis=0)
         R = min(Dim.R, len(idx))
         for r in range(R):
-            roadgraph[a, 0, r, 0] = points.coords[0][0]
-            roadgraph[a, 0, r, 1] = points.coords[0][1]
-            roadgraph[a, 0, r, 2] = points.coords[1][0]
-            roadgraph[a, 0, r, 3] = points.coords[1][1]
+            # Make roadgraph features relative to the agents position
+            roadgraph[a, 0, r, 0] = points[r].coords[0][0] - query.coords[0][0]
+            roadgraph[a, 0, r, 1] = points[r].coords[0][1] - query.coords[0][1]
+            roadgraph[a, 0, r, 2] = points[r].coords[1][0] - query.coords[0][0]
+            roadgraph[a, 0, r, 3] = points[r].coords[1][1] - query.coords[0][1]
             roadgraph[a, 0, r, 4] = agent_data[r][0]
             roadgraph[a, 0, r, 5] = agent_data[r][1]
             roadgraph[a, 0, r, 6] = agent_data[r][2]
@@ -67,12 +73,12 @@ def extract(
 
 def _get_time_index(agent_mask: torch.Tensor, agent_idx: int) -> int | None:
     """
-    We want the closest index at or after t=4, but will go earlier if we have to.
+    We want the closest index at or after t=5, but will go earlier if we have to.
     """
-    for t in range(4, Dim.T):
+    for t in range(5, Dim.T):
         if not agent_mask[agent_idx, t]:
             return t
-    for t in reversed(range(4)):
+    for t in reversed(range(5)):
         if not agent_mask[agent_idx, t]:
             return t
     return None
@@ -92,7 +98,7 @@ def _load_rtree(map_path: Path, reference_point: Tuple[float]):
         geometries.extend(g)
         data.extend(d)
 
-    return shapely.STRtree(geometries), data
+    return shapely.STRtree(geometries), np.array(data)
 
 
 def _extract_lane_segment(
