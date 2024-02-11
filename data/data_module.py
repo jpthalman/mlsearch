4a86bbe6392dbd80412010a1696631460accf20d
Steven Qian <sqian@zoox.com>
2024-02-10 22:45:17 -0800

    feat: setup the data module get item function

--- data/data_module.py
+++ data/data_module.py
@@ -43,22 +43,42 @@ class AV2Dataset(Dataset[Dict[str, torch.Tensor]]):
                 scenario_name=path.name,
                 scenario_path=path / f"scenario_{path.name}.parquet",
                 map_path=path / f"log_map_archive_{path.name}.json",
+                tensors_path=path / "data.pt",
             ))
-
+    """
+    Returns a tensors dictionary of the following forma:
+    dict(
+            scenario_name= str,
+            agent_history= [Dim.A, Dim.T, 1, Dim.S],
+            agent_mask= [Dim.A, Dim.T],
+            agent_interactions= [Dim.A, Dim.T, Dim.Ai, Dim.S],
+            agent_interactions_mask= [Dim.A, Dim.T, Dim.Ai],
+            roadgraph= [Dim.R, Dim.Rd],
+            roadgraph_mask= [Dim.R],
+            ground_truth_controls= [Dim.T, Dim.C],
+        )
+    """
     def __getitem__(self: Self, idx: int) -> Dict[str, torch.Tensor]:
         info = self._paths[idx]
-        # TODO: Populate these tensors
-        controls = torch.rand([Dim.T, Dim.C])
-        return dict(
-            scenario_name=info["scenario_name"],
-            agent_history=torch.zeros([Dim.A, Dim.T, 1, Dim.S]),
-            agent_history_mask=torch.zeros([Dim.A, Dim.T]),
-            agent_interactions=torch.zeros([Dim.A, Dim.T, Dim.Ai, Dim.S]),
-            agent_interactions_mask=torch.zeros([Dim.A, Dim.T, Dim.Ai]),
-            roadgraph=torch.zeros([Dim.R, Dim.Rd]),
-            roadgraph_mask=torch.zeros([Dim.R]),
-            ground_truth_controls=controls,
-        )
+        tensors_dict = torch.load(info[tensors_path])
+
+        # Remove the scenario_name element as it is not in the expected model input.
+        if 'scenario_name' in tensors_dict:
+            del tensors_dict['scenario_name']
+        return tensors_dict
 
     def __len__(self: Self) -> int:
         return len(self._paths)
+
+def main():
+    test_tensors_path = Path("/mnt/sun-tcs02/planner/shared/zRL/jthalman/av2/train/0000b0f9-99f9-4a1f-a231-5be9e4c523f7/data.pt")
+    tensors_dict = torch.load(test_tensors_path)
+    for k, v in tensors_dict.items():
+        print(k)
+        if type(v) == str:
+            print(v)
+        else:
+            print(v.shape)
+
+if __name__ == "__main__":
+    main()
